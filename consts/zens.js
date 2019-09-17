5594b29ec7d89ba7bb8b528e4de1007f18925356
eshoiza <engrjayt200@gmail.com>
2019-09-17 10:52:01 +0100

    first commit

---
+++ consts/zens.js
@@ -0,0 +1,26 @@
+const zens = [
+    {   id: 1,
+        mantra: "Welcome to class",
+        image: "images/hawaii_beach.jpg",
+        audio: "sounds/waves.mp3",
+        text: "Go into a classroom",
+    },
+    {   id: 2,
+        mantra: "Take a Lesson",
+        image: "images/horseshoe_bend.jpg",
+        audio: "sounds/birds.mp3",
+        text: "Learning Asistance",
+    },
+    {   id: 3,
+        mantra: "I am a virtual assistance. I will teach you how to cross road and obey traffic rules",
+        image: "images/sunrise_paris_2.jpg",
+        audio: "sounds/chimes.mp3",
+        text: "Road Crossing assistance",
+    },
+    {   id: 4,
+        image: "images/homebase.jpg",
+        text: "Home"
+    }
+]
+
+export default zens;
---
+++ scenes/home-environment/components/zen-button/style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-360";
+
+export default StyleSheet.create({
+    text: {
+        backgroundColor: "#332200",
+        textAlign: "center",
+        color: "white",
+        marginTop: 30  
+    }
+})
